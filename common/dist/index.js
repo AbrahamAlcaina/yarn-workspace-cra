f77576154555a0288cd8900baa12d5f03d47791c
Jack Zhao <yzhao@grailbio.com>
2018-10-12 09:35:09 -0700

    initial commit

---
+++ common/dist/index.js
@@ -0,0 +1,29 @@
+import _classCallCheck from "/Users/jzhao/dev/yarn-workspace-cra/node_modules/@babel/runtime/helpers/esm/classCallCheck";
+import _createClass from "/Users/jzhao/dev/yarn-workspace-cra/node_modules/@babel/runtime/helpers/esm/createClass";
+import _possibleConstructorReturn from "/Users/jzhao/dev/yarn-workspace-cra/node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn";
+import _getPrototypeOf from "/Users/jzhao/dev/yarn-workspace-cra/node_modules/@babel/runtime/helpers/esm/getPrototypeOf";
+import _inherits from "/Users/jzhao/dev/yarn-workspace-cra/node_modules/@babel/runtime/helpers/esm/inherits";
+import React from 'react';
+
+var Test =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Test, _React$Component);
+
+  function Test() {
+    _classCallCheck(this, Test);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Test).apply(this, arguments));
+  }
+
+  _createClass(Test, [{
+    key: "render",
+    value: function render() {
+      return React.createElement("h1", null, " hellooooo ");
+    }
+  }]);
+
+  return Test;
+}(React.Component);
+
+export { Test as default };
---
+++ common/webpack.config.js
@@ -0,0 +1,23 @@
+// @flow
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  externals: [
+    nodeExternals({
+      modulesFromFile: true,
+      whitelist: [/^common\//],
+    }),
+  ],
+};
